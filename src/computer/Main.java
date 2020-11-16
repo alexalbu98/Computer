44001e629a67013016cb9cdbc96e45fa27a3323e
alexalbu <alexalbu78@yahoo.com>
2020-11-16 20:02:16 +0200

    Create processor class

--- src/computer/Main.java
+++ src/computer/Main.java
@@ -1,8 +1,13 @@
 package computer;
+import computer.processors.IntelProcessor;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        IntelProcessor my_processor = new IntelProcessor(2000, 4);
+        System.out.println(my_processor.add(2,3));
+        System.out.println(my_processor.devide(4,2));
+
     }
 }
---
+++ src/computer/processors/IntelProcessor.java
@@ -0,0 +1,67 @@
+package computer.processors;
+import computer.interfaces.Processor;
+
+public class IntelProcessor implements Processor {
+
+    private int processor_speed;
+    private int n_cores;
+    private final String type;
+
+    public IntelProcessor(){
+        type = "Intel";
+    }
+    public IntelProcessor(int speed, int cores)
+    {
+        processor_speed = speed;
+        n_cores = cores;
+        type = "Intel";
+    }
+
+    @Override
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    @Override
+    public int devide(int a, int b) {
+        return a/b;
+    }
+
+    @Override
+    public int to_power(int number, int pwr) {
+        int result = 1;
+        for(int i=0; i< pwr ; i++)
+        {
+            result  = result*number;
+        }
+        return  result;
+    }
+
+    /**
+     * Executes a process on one core
+     * @return returns the execution code
+     */
+    @Override
+    public int run_process() {
+        return 0;
+    }
+
+    //Getter region
+    public int getN_cores() {
+        return n_cores;
+    }
+
+    public int getProcessor_speed() {
+        return processor_speed;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}

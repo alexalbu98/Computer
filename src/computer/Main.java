9b0a5d05efda02ae3158d0b3f2ff3377adc830f6
lacramioara <runcan_lacramioara@yahoo.com>
2020-11-17 23:50:04 +0200

    Am testat functionalitatile clasei  computer.
    Signed-off-by: lacramioara <runcan_lacramioara@yahoo.com>

--- src/computer/Main.java
+++ src/computer/Main.java
@@ -14,9 +14,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ROM my_rom= new ROM(200,400);
+        computerClass my_computer=new computerClass(my_rom);
+        SSD my_ssd =new SSD(100,200);
+        HDD my_hdd =new HDD(200,300);
+        RAM my_ram=new RAM(500,200);
+        CPU_Process my_cpu=new CPU_Process("test","test");
+        IntelProcessor my_intel =new IntelProcessor(500,4);
+        IntelProcessor new_intel=new IntelProcessor(200,2);
+        my_computer.add_hdd(my_hdd);
+        my_computer.add_ssd(my_ssd);
+        my_computer.add_process(my_cpu);
+        my_computer.add_processor(my_intel);
+        my_computer.add_ram(my_ram);
 
-        Application app = new Cat("C:\\Users\\Work\\IdeaProjects\\Project\\LICENSE");
-        app.execute();
+        my_computer.delete_processor(new_intel);
 
     }
 }
